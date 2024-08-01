dd8e220b24f469c361fa6a7347a8c77a1bd5c1d3
martin <m.cepauskas@varutis.lt>
2024-08-01 11:39:25 +0300

    fixed dropdown and some style

--- src/app/shared/store/Employee/Employee.model.ts
+++ src/app/shared/store/Employee/Employee.model.ts
@@ -9,4 +9,4 @@ export enum Level {
     junior = 1, mid = 2, senior = 3
 }
 
-export const LevelOptions = [{id: Level.junior,name: 'junior'}, {id: Level.mid, name: 'mid'}, {id: 3, name: Level.senior}];
+export const LevelOptions = [{id: Level.junior,name: 'junior'}, {id: Level.mid, name: 'mid'}, {id: 3, name: 'senior'}];
